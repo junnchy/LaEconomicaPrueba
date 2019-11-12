06445eddc96fe159ee809450abf56a5c11e50d44
junnchy <juanichacho@gmail.com>
2019-11-12 12:14:15 -0300

    merge

--- database/seeds/CondicionIvaTableSeeder.php
+++ database/seeds/CondicionIvaTableSeeder.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
+use App\CondicionIva;
 
 
 class CondicionIvaTableSeeder extends Seeder
@@ -13,14 +14,14 @@ class CondicionIvaTableSeeder extends Seeder
      */
     public function run()
     {
-        $tabla = DB::table('condicion_iva');
-        $tabla->truncate();
-        $tabla->insert(['denominacion'=>'Responsable Inscripto']);
-        $tabla->insert(['denominacion'=>'Responsable No Inscripto']);
-        $tabla->insert(['denominacion'=>'Consumidor Final']);
-        $tabla->insert(['denominacion'=>'Exento']);
-        $tabla->insert(['denominacion'=>'Monotributista']);
-        $tabla->insert(['denominacion'=>'No Categorizado']);
+        
+        CondicionIva::truncate();
+        CondicionIva::insert(['denominacion'=>'Responsable Inscripto']);
+        CondicionIva::insert(['denominacion'=>'Responsable No Inscripto']);
+        CondicionIva::insert(['denominacion'=>'Consumidor Final']);
+        CondicionIva::insert(['denominacion'=>'Exento']);
+        CondicionIva::insert(['denominacion'=>'Monotributista']);
+        CondicionIva::insert(['denominacion'=>'No Categorizado']);
         
     }
 }
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,15 +11,15 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {   
+        $this->call(CategoriasSeeder::class);
+        $this->call(ProveedorTableSeeder::class);
+
         DB::statement('SET FOREIGN_KEY_CHECKS=0;');
         $this->call(CondicionIvaTableSeeder::class);
         $this->call(RubroProveedoresTableSeeder::class);
         $this->call(CategoriaClientesTableSeeder::class);
         $this->call(LocalidadesTableSeeder::class);
-        $this->call(CategoriasSeeder::class);
-        $this->call(ProveedorTableSeeder::class);
         DB::statement('SET FOREIGN_KEY_CHECKS=1;');
         
-      
     }
 }
